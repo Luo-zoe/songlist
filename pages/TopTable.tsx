8d019d76999c807fefe4f81eef35abab33796edc
zzy <475452600@qq.com>
2024-07-12 10:21:45 +0800

    add song list

--- pages/TopTable.tsx
+++ pages/TopTable.tsx
@@ -1,17 +1,50 @@
 import React, { useState } from "react";
-import { Input } from "@nextui-org/react";
+import { Input, Autocomplete, AutocompleteItem } from "@nextui-org/react";
 
 import SearchIcon from "./SearchIcon";
 
-const TopTable = ({ onSearch }) => {
+const TopTable = ({
+  data,
+  onSearch,
+  onSelectSong,
+  onSelectLanguage,
+  onSelectStyle,
+}) => {
   const [query, setQuery] = useState<string>();
 
+  let songList = [];
+  let styleList = [];
+  let languageList = [];
+
+  if (data && Array.isArray(data)) {
+    songList = Array.from(new Set(data.map((value) => value.song))).map(
+      (song) =>
+        ({
+          label: song,
+          value: song,
+        } as any)
+    );
+    styleList = Array.from(new Set(data.map((value) => value.style))).map(
+      (style) =>
+        ({
+          label: style,
+          value: style,
+        } as any)
+    );
+    languageList = Array.from(new Set(data.map((value) => value.language))).map(
+      (language) =>
+        ({
+          label: language,
+          value: language,
+        } as any)
+    );
+  }
   return (
-    <div className="w-full flex flex-col gap-4 my-6 px-4">
-      <div className="flex gap-3 items-end">
+    <div className="w-full flex flex-col md:flex-row gap-4 my-6 px-0 items-center">
+      <div className="w-full md:w-1/2">
         <Input
           isClearable
-          className="w-full border-none"
+          className="w-full"
           placeholder="搜索歌名"
           startContent={<SearchIcon />}
           value={query}
@@ -21,6 +54,44 @@ const TopTable = ({ onSearch }) => {
             onSearch(e);
           }}
         />
+      </div>
+      <div className="flex w-full md:w-1/2 gap-1">
+        <Autocomplete
+          className="h-10"
+          placeholder="全部歌手"
+          isClearable={false}
+          onSelectionChange={onSelectSong}
+        >
+          {songList.map((animal) => (
+            <AutocompleteItem key={animal.value} value={animal.value}>
+              {animal.label}
+            </AutocompleteItem>
+          ))}
+        </Autocomplete>
+        <Autocomplete
+          placeholder="全部风格"
+          isClearable={false}
+          className="h-10"
+          onSelectionChange={onSelectStyle}
+        >
+          {styleList.map((animal) => (
+            <AutocompleteItem key={animal.value} value={animal.value}>
+              {animal.label}
+            </AutocompleteItem>
+          ))}
+        </Autocomplete>
+        <Autocomplete
+          placeholder="全部语言"
+          isClearable={false}
+          className="h-10"
+          onSelectionChange={onSelectLanguage}
+        >
+          {languageList.map((animal) => (
+            <AutocompleteItem key={animal.value} value={animal.value}>
+              {animal.label}
+            </AutocompleteItem>
+          ))}
+        </Autocomplete>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { use, useCallback, useEffect, useMemo, useState } from "react";
+import React, { Component } from "react";
 import { NextPage } from "next";
 import {
   Table,
@@ -11,18 +11,47 @@ import {
   Card,
   CardBody,
   Image,
-  Button,
-  Slider,
 } from "@nextui-org/react";
 import TopTable from "./TopTable";
 import XLSX from "xlsx";
 import toast from "react-hot-toast";
 
-const Home: NextPage = () => {
-  const [data, setData] = useState([]);
-  const [search, setSearch] = useState([]);
+interface SongData {
+  key: string;
+  name: string;
+  song: string;
+  language: string;
+  style: string;
+  remark: string;
+}
 
-  const fetchData = async () => {
+interface ParentComponentState {
+  data: SongData[];
+  search: SongData[];
+  nameQuery: string;
+  songQuery: string;
+  styleQuery: string;
+  languageQuery: string;
+}
+class Home extends Component<{}, ParentComponentState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      search: [],
+      nameQuery: "",
+      songQuery: "",
+      styleQuery: "",
+      languageQuery: "",
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
     try {
       const response = await fetch("/song.xlsx");
       const arrayBuffer = await response.arrayBuffer();
@@ -38,136 +67,178 @@ const Home: NextPage = () => {
         style: row[3] || "",
         remark: row[4] || "",
       }));
-      setData(formattedData);
-      setSearch(formattedData);
-      // console.info("----", formattedData);
+      this.setState({
+        data: formattedData,
+        search: formattedData,
+      });
     } catch (error) {
       console.error("Error fetching and processing the file", error);
     }
+  }
+
+  handleSearch = () => {
+    const { nameQuery, songQuery, languageQuery, styleQuery, data } =
+      this.state;
+    let filteredData = data;
+
+    if (nameQuery) {
+      filteredData = filteredData.filter((item) =>
+        item.name.includes(nameQuery)
+      );
+    }
+
+    if (songQuery) {
+      filteredData = filteredData.filter((item) =>
+        item.song.includes(songQuery)
+      );
+    }
+
+    if (styleQuery) {
+      filteredData = filteredData.filter((item) =>
+        item.style.includes(styleQuery)
+      );
+    }
+
+    if (languageQuery) {
+      filteredData = filteredData.filter((item) =>
+        item.language.includes(languageQuery)
+      );
+    }
+    this.setState({ search: filteredData });
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  handleNameChange = (query) => {
+    this.setState({ nameQuery: query }, this.handleSearch);
+  };
+  handleSongChange = (query) => {
+    this.setState({ songQuery: query }, this.handleSearch);
+  };
 
-  const handleSearch = useCallback((query) => {
-    if (query === "") {
-      setSearch(data);
-    } else {
-      setSearch(data.filter((item) => item.name.includes(query)));
-    }
-  }, []);
+  handleStyleChange = (query) => {
+    this.setState({ styleQuery: query }, this.handleSearch);
+  };
+  handleLanguageChange = (query) => {
+    this.setState({ languageQuery: query }, this.handleSearch);
+  };
 
-  return (
-    <div>
-      <div className="fixed w-full h-full top-0 left-0 -z-10 object-cover bg-[url(/11.jpg)] bg-cover bg-center bg-no-repeat" />
-      <div className="flex flex-col items-center pt-10">
-        <Card
-          isBlurred
-          className="border-none bg-background/60 dark:bg-default-100/50 sm:w-[70%] w-full"
-          shadow="sm"
-        >
-          <CardBody>
-            <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center w-full">
-              <div className="relative col-span-6 md:col-span-4 justify-self-center">
-                <Image
-                  alt="Album cover"
-                  className="object-cover"
-                  height={200}
-                  shadow="sm"
-                  src="/avatar.png"
-                />
-              </div>
-              <div className="flex flex-col col-span-6 md:col-span-8">
-                <div className="flex justify-between items-start">
-                  <div className="flex flex-col gap-0">
-                    <h3 className="font-semibold text-foreground/90">
-                      欢迎来到阿胖哒Apanda的歌单
-                    </h3>
-                    <a
-                      className="text-blue-500 no-underline mt-3"
-                      href="https://live.bilibili.com/32503794"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      阿胖哒Apanda直播间入口
-                    </a>
-                    <ul className="list-disc pl-5 space-y-3 text-left mt-3">
-                      <li>带牌子发弹幕/情书点歌：点歌+歌名</li>
-                      <li>每次点歌冷却1分钟</li>
-                      <li>SC置顶歌曲</li>
-                      <li>舰长点歌无限制（7分钟以上的歌限舰长可点）</li>
-                    </ul>
+  render() {
+    return (
+      <div>
+        <div className="fixed w-full h-full top-0 left-0 -z-10 object-cover bg-[url(/11.jpg)] bg-cover bg-center bg-no-repeat" />
+        <div className="flex flex-col items-center py-10">
+          <Card
+            isBlurred
+            className="border-none bg-background/60 dark:bg-default-100/50 sm:w-[70%] w-full"
+            shadow="sm"
+          >
+            <CardBody>
+              <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center w-full">
+                <div className="relative col-span-6 md:col-span-4 justify-self-center">
+                  <Image
+                    alt="Album cover"
+                    className="object-cover"
+                    height={200}
+                    shadow="sm"
+                    src="/avatar.png"
+                  />
+                </div>
+                <div className="flex flex-col col-span-6 md:col-span-8">
+                  <div className="flex justify-between items-start">
+                    <div className="flex flex-col gap-0">
+                      <h3 className="font-semibold text-foreground/90">
+                        欢迎来到阿胖哒Apanda的歌单
+                      </h3>
+                      <a
+                        className="text-blue-500 no-underline mt-3"
+                        href="https://live.bilibili.com/32503794"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        阿胖哒Apanda直播间入口
+                      </a>
+                      <ul className="list-disc pl-5 space-y-3 text-left mt-3">
+                        <li>带牌子发弹幕/情书点歌：点歌+歌名</li>
+                        <li>每次点歌冷却1分钟</li>
+                        <li>SC置顶歌曲</li>
+                        <li>舰长点歌无限制（7分钟以上的歌限舰长可点）</li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </CardBody>
-        </Card>
-        <Card
-          isBlurred
-          className="border-none bg-background/60 dark:bg-default-100/50 mt-10 md:w-[70%] sm:w-full"
-          shadow="sm"
-        >
-          <CardBody>
-            <TopTable onSearch={handleSearch} />
-            <Table
-              // isStriped
-              isHeaderSticky
-              removeWrapper
-              selectionMode="single"
-              color="primary"
-              className="max-h-[50vh] w-full overflow-auto border bg-white bg-opacity-60 rounded-xl"
-            >
-              <TableHeader>
-                <TableColumn key="name">歌名</TableColumn>
-                <TableColumn key="song">歌手</TableColumn>
-                <TableColumn key="language" className="sm:w-1/4 md:w-1/12">
-                  语言
-                </TableColumn>
-                <TableColumn
-                  key="style"
-                  className="hidden sm:table-cell md:w-1/12"
-                >
-                  样式
-                </TableColumn>
-                <TableColumn
-                  key="remark"
-                  className="hidden sm:table-cell md:w-auto"
-                >
-                  备注
-                </TableColumn>
-              </TableHeader>
-              <TableBody items={search}>
-                {(item) => (
-                  <TableRow
-                    key={item.key}
-                    onClick={() => {
-                      navigator.clipboard.writeText(`点歌 ${item.name}`);
-                      toast.success(`复制成功! 点歌：${item.name}`, {
-                        duration: 2000,
-                      });
-                    }}
+            </CardBody>
+          </Card>
+          <Card
+            isBlurred
+            className="border-none bg-background/60 dark:bg-default-100/50 mt-10 sm:w-[70%] w-full"
+            shadow="sm"
+          >
+            <CardBody>
+              <TopTable
+                onSearch={this.handleNameChange}
+                onSelectSong={this.handleSongChange}
+                onSelectStyle={this.handleStyleChange}
+                onSelectLanguage={this.handleLanguageChange}
+                data={this.state.data}
+              />
+              <Table
+                // isStriped
+                isHeaderSticky
+                removeWrapper
+                selectionMode="single"
+                color="primary"
+                className="max-h-[60vh] w-full overflow-auto border bg-white bg-opacity-60 rounded-xl"
+              >
+                <TableHeader>
+                  <TableColumn key="name">歌名</TableColumn>
+                  <TableColumn key="song">歌手</TableColumn>
+                  <TableColumn key="language" className="sm:w-1/4 md:w-1/12">
+                    语言
+                  </TableColumn>
+                  <TableColumn
+                    key="style"
+                    className="hidden sm:table-cell md:w-1/12"
                   >
-                    {(columnKey) => (
-                      <TableCell
-                        className={`${
-                          columnKey === "style" || columnKey === "remark"
-                            ? "hidden sm:table-cell"
-                            : ""
-                        }`}
-                      >
-                        {getKeyValue(item, columnKey)}
-                      </TableCell>
-                    )}
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </CardBody>
-        </Card>
+                    样式
+                  </TableColumn>
+                  <TableColumn
+                    key="remark"
+                    className="hidden sm:table-cell md:w-auto"
+                  >
+                    备注
+                  </TableColumn>
+                </TableHeader>
+                <TableBody items={this.state.search}>
+                  {(item) => (
+                    <TableRow
+                      key={item.key}
+                      onClick={() => {
+                        navigator.clipboard.writeText(`点歌 ${item.name}`);
+                        toast.success(`复制成功! 点歌：${item.name}`, {
+                          duration: 2000,
+                        });
+                      }}
+                    >
+                      {(columnKey) => (
+                        <TableCell
+                          className={`${
+                            columnKey === "style" || columnKey === "remark"
+                              ? "hidden sm:table-cell"
+                              : ""
+                          }`}
+                        >
+                          {getKeyValue(item, columnKey)}
+                        </TableCell>
+                      )}
+                    </TableRow>
+                  )}
+                </TableBody>
+              </Table>
+            </CardBody>
+          </Card>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default Home;
